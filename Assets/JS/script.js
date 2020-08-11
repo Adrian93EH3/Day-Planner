a56ad2279aaacb6e3e326ede77ed3667587a0cb5
Adrian Diaz <adrian93eh3@gmail.com>
2020-08-11 15:17:50 -0400

    adding the data function for the header date

---
+++ Assets/JS/script.js
@@ -0,0 +1,74 @@
+// Variable to store values for my 9-5 schedule
+var timeblock = [
+
+    {
+        id: "0",
+        hour: "09",
+        time: "09",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "1",
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "3",
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "1",
+        time: "1",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "2",
+        time: "2",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "6",
+        hour: "3",
+        time: "3",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "7",
+        hour: "4",
+        time: "4",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "8",
+        hour: "5",
+        time: "5",
+        meridiem: "pm",
+        reminder: ""
+    },
+
+]
+
+// Gets data for the header date
+function getHeaderDate() {
+    var currentHeaderDate = moment().format('dddd, MMMM Do');
+    $("#currentDay").text(currentHeaderDate);
+}

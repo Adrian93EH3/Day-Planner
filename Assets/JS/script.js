e0e9461f830c608331b8b49da5a5a7ff97c36bd5
Adrian Diaz <adrian93eh3@gmail.com>
2020-09-03 14:07:29 -0400

    finishing touches for the resubmission

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,18 +1,24 @@
 // GLOBAL VARIABLES
-var hour = moment().format("H")
-var button = $(".saveBtn") 
-
+// ====================================
+const hour = moment().format("H")
+const button = $(".saveBtn")
 var moment = moment();
-var presentDay = moment.format('dddd, MMMM Do, YYYY');
+
+// CODE TO SHOW DATE AND YEAR OF PRESENT DAY IN JUMBOTRON
+// ==============================================================
+const presentDay = moment.format('dddd, MMMM Do, YYYY');
 $("#presentDay").text(presentDay);
 
 // ON-CLICK EVENT FOR SAVING TASKS FOR THAT TIME
+// =======================================================
 button.on("click", function () {
-    var userInput = $(this).siblings(".tasks").val();
-        var mainRow = $(this).parent().attr("id");
-        localStorage.setItem(mainRow, userInput)
+    const userInput = $(this).siblings(".tasks").val();
+    const mainRow = $(this).parent().attr("id");
+    localStorage.setItem(mainRow, userInput)
 });
 
+// GIVES EACH BLOCK THE TEXT THAT WAS LAST SAVED INTO THE LOCALSTORAGE
+// ===========================================================================
 $("#9 .tasks").val(localStorage.getItem("9"))
 $("#10 .tasks").val(localStorage.getItem("10"))
 $("#11 .tasks").val(localStorage.getItem("11"))
@@ -22,3 +28,22 @@ $("#14 .tasks").val(localStorage.getItem("14"))
 $("#15 .tasks").val(localStorage.getItem("15"))
 $("#16 .tasks").val(localStorage.getItem("16"))
 $("#17 .tasks").val(localStorage.getItem("17"))
+
+// PAST, PRESENT, FUTURE FUNCTION FOR THE TIMEBLOCKS
+// ==========================================================
+function currentTime() {
+    $(".time-block").each(function () {
+        var divId = $(this).attr("id")
+        var divTime = parseInt(divId);
+        if(divTime < hour) {
+            $(this).children(".tasks").attr("class", "col-md-10 tasks past");
+        } else if (divTime == hour) {
+            $(this).children(".tasks").attr("class", "col-md-10 tasks present");
+        } else {
+            $(this).children(".tasks").attr("class", "col-md-10 tasks future");
+        };
+    });
+
+};
+    
+currentTime();
